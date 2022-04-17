79ec427819a5d85dd751f616c61f8567f2fdb54e
Семен <25564222+SaimonFenix@users.noreply.github.com>
2022-04-17 14:28:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+    $('.header-slider').slick({
+        dots: true,
+        vertical: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="images/up.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="images/next.svg" alt="arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                }
+            }
+        ]
+    });
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"> <img src="images/product-prev.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="product-next"> <img src="images/product-next.svg" alt="arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 891,
+                settings: {
+                    vertical: false,
+                    slidesToShow: 3,
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        vertical: true,
+        arrows: false
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+});
